106877e477364a25e8e298f0a2242cad68003cc2
Deelolade <oluwanisholahabeeb13@icloud.com>
2025-11-02 23:30:53 +0100

    authentication: created the signin page too

--- app/(auth)/signin/page.tsx
+++ app/(auth)/signin/page.tsx
@@ -1,11 +1,43 @@
+import Image from 'next/image';
 import React from 'react'
-
+import signUpImage from "@/public/images/signUpImage.jpg"
+import Link from 'next/link';
 const page = () => {
-  return (
-    <div>
-      signin page
-    </div>
-  )
+    return (
+        <>
+            <section className="flex justify-center items-center h-screen">
+                <div className=" flex justify-center items-center h-screen w-screen ">
+                    <div className="w-1/2 flex justify-center items-center">
+                        <form className='w-[400px] p-8 shadow-lg rounded-lg'>
+                            <div className=" text-center">
+                                <h1 className='text-2xl font-semibold mb-3'>Sign In</h1>
+                                <p className='text-sm text-gray-500 font-semibold'>Welcome back! Enter your email and password to access your account.</p>
+                            </div>
+                            <div className="flex flex-col gap-5 mt-6">
+                                <div className="flex flex-col">
+                                    <label htmlFor="" className='text-sm mb-2 font-semibold'>Email Address</label>
+                                    <input type="email" className='border border-gray-300 px-3 py-1 rounded-md font-semibold outline-0' placeholder="e.g. johndoe@example.com" />
+                                </div>
+                                <div className="flex flex-col">
+                                    <label htmlFor="" className='text-sm mb-2 font-semibold'>Password</label>
+                                    <input type="password" className='border border-gray-300 px-3 py-1 rounded-md font-semibold outline-0' placeholder="Enter your password" />
+                                </div>
+                                <div className="">
+                                    <button className='bg-black text-white text-sm w-full py-2 rounded-lg font-semibold'>Sign In</button>
+                                    <div className="mt-5 ">
+                                        <p className=' text-center text-sm font-semibold'>New here? <Link href="/signup" className='hover:underline'>Sign Up</Link></p>
+                                    </div>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                <div className="w-1/2 bg-green-200 h-screen flex justify-center items-center">
+                    <Image src={signUpImage} alt='PDF_EXTARCTOR_IMAGE' className='w-full h-full object-cover' />
+                </div>
+                </div>
+            </section>
+        </>
+    )
 }
 
-export default page
+export default page;
